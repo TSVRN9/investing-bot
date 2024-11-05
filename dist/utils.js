f75e295eacfe847aaf08a668c3188c8e98aa9865
TSVRN9 <66744142+TSVRN9@users.noreply.github.com>
2024-11-04 20:21:59 -0500

    Remove dist from git

--- dist/utils.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.choose = choose;
-function choose(possibilities) {
-    return possibilities[Math.floor(possibilities.length * Math.random())];
-}
